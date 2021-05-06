91b0b237012abd4bdec9c65b2063108837cbd8f6
NyW8 <tobluee@gmail.com>
2021-05-06 19:03:35 -0400

    Get links, setup, and preview

---
+++ assets/js/terminal.js
@@ -0,0 +1,226 @@
+$(function() {
+	$('.terminal').on('click', function() {
+  	$('#input').focus();
+  });
+
+  var currentDir = null;
+  var loaderHTML = "<div id=\"loader-parent\"><div class=\"loader\"></div></div>";
+
+  $('#overlay').on('click', function() {
+    setOverlay("none");
+  });
+
+  $('#info-icon').on('click', function() {
+    setOverlay("flex");
+  });
+
+  var writeHistory = function(toWrite) {
+    $('#history').append(toWrite + '<br/>');
+  };
+
+  var clearInput = function() {
+    $('#input').val('');
+  };
+
+  var setOverlay = function (displayValue) {
+    $('#overlay').css("display", displayValue);
+  };
+
+  $('#input').on('keydown',function search(e) {
+    setOverlay("none");
+		if(e.keyCode == 13) {
+      var inputString = $(this).val();
+      var arguments = inputString.split(" ");
+      writeHistory(currentDir.pwd() + '&nbsp;>&nbsp;' + inputString);
+      var cmd = arguments.shift();
+      var preview_half = $('#preview_half');
+      preview_half.css("display", "none");
+      
+      if(cmd === 'cd') {
+        var path = arguments[0];
+        var errMsg = "cd: " + path + ": No such directory";
+        var result = currentDir.cd(path);
+        
+        if (result == null) {
+          writeHistory(errMsg);
+          clearInput();
+          return;
+        }
+
+        currentDir = result;
+        $('#path').html(currentDir.pwd()+'&nbsp;>&nbsp;');
+      } else if (cmd === "ls") {
+        var path = arguments.length > 0 ? arguments[0] : ".";
+        var errMsg = "ls: " + path + ": No such directory";
+        var result = currentDir.cd(path);
+
+        if (result == null) { 
+          writeHistory(errMsg);
+          clearInput();
+          return;
+        }
+
+        writeHistory(result.ls());
+      } else if (cmd == "cat") {
+        var path = arguments.length > 0 ? arguments[0] : ".";
+        var errMsg = "cat: " + path + ": No such file or link";
+        var splitPath = path.split("/");
+        var result = currentDir.goToPath(splitPath, false);
+
+        if (result == null || result.type == "dir") {
+          writeHistory(errMsg);
+          clearInput();
+          return;
+        }
+
+        writeHistory(result.cat());
+        if (result.type == "file") {
+          var elem = document.getElementById("preview_half");
+          elem.innerHTML = loaderHTML;
+          result.getContent(elem);
+          preview_half.show();
+        }
+      } else if (arguments[0] == "clear") {
+        $('#history').empty();
+      } else {
+        writeHistory(cmd + ": Command not recognized");
+      }
+      $('.terminal_half').scrollTop($('.terminal_half')[0].scrollHeight);
+      
+      clearInput();
+    }
+  });
+
+  var parsePath = function(dir, splitPath) {
+    splitPath = splitPath.filter(i => i !== ".");
+    if (splitPath.length == 0)
+      return dir;
+    var oneLeft = (splitPath.length == 1); // only one file/dir down
+
+    if (splitPath[0] === ".." && dir.parentDir != null) {
+      if (oneLeft)
+        return dir.parentDir;
+      splitPath.shift();
+      return parsePath(dir.parentDir, splitPath);
+    } else if (splitPath[0] === "..") {
+      return null;
+    }
+  
+    var children = dir.children;
+    for (var i = 0; i < dir.children.length; i++) {
+      if (children[i].name == splitPath[0]) {
+        splitPath.shift();
+        return children[i].goToPath(splitPath, oneLeft);
+      } 
+    }
+  }
+  
+  var setupFileSystem = function () {
+    // want to show folder for projects, coursework, files for each: about, resume, events, contact, "soft links" for gitlab, linkedin
+    var root = new Dir("", null);
+    var rootChildren = [new Dir("projects", root), new Dir("coursework", root),
+      new FileEntry("about", "https://nyw8.github.io/about/", "about"), new FileEntry("resume", "https://nyw8.github.io/resume/", "resume"),
+      new FileEntry("events", "https://nyw8.github.io/events/", "events"), new FileEntry("contact", "https://nyw8.github.io/contact/", "container_contact"),
+      new Link("github", "https://github.com/NyW8"), new Link("linkedin", "https://www.linkedin.com/in/nyah-way/")];
+    for (var i = 0; i < rootChildren.length; i++) {
+      root.addChild(rootChildren[i]);
+    }
+    var projectChildren = []
+
+    currentDir = root;
+    $('#path').html(currentDir.pwd()+'&nbsp;>&nbsp;');
+  }
+  
+  class Dir {
+    constructor(name, parentDir) {
+      this.name = name;
+      this.parentDir = parentDir;
+      this.children = [];
+      this.type = "dir";
+    }
+  
+    addChild (child) {
+      this.children.push(child);
+    }
+  
+    pwd () {
+      if (this.parentDir == null) {
+        return "/";
+      }
+      return this.parentDir.pwd() + this.name;
+    }
+  
+    cd (path) {
+      var location = parsePath(this, path.split('/'));
+      if (location == null || location.type != "dir")
+        return null;
+      
+      return location;
+    }
+  
+    ls () {
+      var output = this.parentDir == null ? "." : ".<br>..";
+      for (var i = 0; i < this.children.length; i++) {
+        output = output + "<br>" + this.children[i].name;
+      }
+      return output;
+    }
+  
+    goToPath (splitPath, last) {
+      return last ? this : parsePath(this, splitPath);
+    }
+
+    cat () {
+      return null;
+    }
+  }
+  
+  class FileEntry {
+    constructor (name, contentUrl, classToShow) {
+      this.name = name + ".txt";
+      this.contentUrl = contentUrl;
+      this.classToShow = classToShow;
+      this.type = "file";
+    }
+  
+    goToPath(splitPath, last) {
+      return last ? this : null;
+    }
+  
+    cat () {
+      return "View the requested content on the right >";
+    }
+
+    getContent(elem) {
+      var classToShow = "." + this.classToShow;
+      $.ajax({
+        url: this.contentUrl,
+        type:'GET',
+        success: function(data) {
+          var section = $(data).find(classToShow).parent();
+          var htmlString = section.html();
+          elem.innerHTML = htmlString;
+        }
+     });
+    } 
+  }
+
+  class Link {
+    constructor (name, url) {
+      this.name = name + ".ln";
+      this.url = url;
+      this.type = "link";
+    }
+  
+    goToPath(splitPath, last) {
+      return last ? this : null;
+    }
+  
+    cat () {
+      window.open(this.url, "_blank");
+      return "Link opened in new tab";
+    }
+  }
+
+  setupFileSystem();
+});
